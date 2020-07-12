fe145aab4647dc2b6f3cb81834cacabf3f205184
Berezkin Kirill <killer.kir@inbox.ru>
2020-07-12 23:23:56 +0600

    add db

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "mongouri": "mongodb://localhost/test_test",
+    "mongouri": "mongodb+srv://kirill:19372882@cluster0-tmjbm.mongodb.net/mernLinks?retryWrites=true&w=majority",
     "jwtSecret": "JwtsecretJwt",
     "baseUrl": "http://localhost:5000"
 }
