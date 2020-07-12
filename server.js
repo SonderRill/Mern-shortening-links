62b7cb3ed44a0a7734a1321695e9c435f9bf9e3a
Berezkin Kirill <killer.kir@inbox.ru>
2020-07-12 23:43:53 +0600

    c

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ app.use('/t', require('./routes/redirect'))
 app.use('/auth', require('./routes/auth'))
 app.use('/link', require('./routes/link'))
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server has been started on ${PORT}`))
